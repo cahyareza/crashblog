467a588e7f8e23c348de4cf11ee25386fd33ccdd
cahyafaisalreza <cf.reza07@gmail.com>
2021-10-19 01:51:05 +0700

    Customize admin
    - add search, filters, inlines and similar.

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from .models import Post, Comment, Category
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(Category)
+class CommentItemInline(admin.TabularInline):
+	model = Comment
+	raw_id_fields = ['post']
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'intro', 'body']
+    list_display = ['title', 'slug', 'category', 'created_at']
+    list_filter = ['category','created_at']
+    inlines = [CommentItemInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+	search_fields = ['title']
+	list_display = ['title']
+
+class CommentAdmin(admin.ModelAdmin):
+	list_display = ['name', 'post', 'created_at']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
